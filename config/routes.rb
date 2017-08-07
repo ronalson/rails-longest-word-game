215e591cdba9a77daa91776940748b57526d44d8
Ronalson Filho <contact@ronalson.com.br>
2017-08-07 17:19:04 -0300

    Update router to user /game and /score

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'matches/new'
+  get 'game', to: 'matches#new'
 
-  get 'matches/show'
+  get 'score', to: 'matches#show'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
